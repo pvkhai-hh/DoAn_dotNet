daab9a66c351a7a7cd87fc1ec8e60d39b69829db
pvkhai-hh <phanvankhai18102005@gmail.com>
2025-11-22 12:31:03 +0700

    a

---
+++ DoAn_DotNet/ChuoiKetNoi - Copy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAn_DotNet
+{
+    internal class ChuoiKetNoicoppy
+    {
+        // xóa đi chữ coppy và đổi tên máy ( ADMIN\SQLEXPRESS  thành tên máy của mình)
+        public string strKetNoi = @"Data Source=ADMIN\SQLEXPRESS;Initial Catalog=QLSANBONG;Integrated Security=True";
+    }
+}
